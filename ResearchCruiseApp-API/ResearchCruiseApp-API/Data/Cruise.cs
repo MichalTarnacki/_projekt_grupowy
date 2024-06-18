07bdbe413ac8b059ed97d66424c281c0bb93e585
Mateusz Kowalczyk <git.user@sspg.pl>
2024-06-18 18:59:30 +0200

    Added cruises auto-adding

--- ResearchCruiseApp-API/ResearchCruiseApp-API/Data/Cruise.cs
+++ ResearchCruiseApp-API/ResearchCruiseApp-API/Data/Cruise.cs
@@ -14,9 +14,9 @@ public class Cruise : IYearBasedNumberedEntity
 
     public Guid MainDeputyManagerId { get; set; }
 
-    public DateOnly StartDate { get; set; }
+    public DateTime StartDate { get; set; }
 
-    public DateOnly EndDate { get; set; }
+    public DateTime EndDate { get; set; }
 
     public List<Application> Applications { get; set; } = null!;
 }
