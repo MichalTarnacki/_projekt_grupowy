ebcafe50094ef05a6910021a226c0312adbd48f3
Mateusz Kowalczyk <mateuszkowalczyk.mko@gmail.com>
2024-08-18 21:17:12 +0200

    Scan size is validated in backend

--- ResearchCruiseApp-API/ResearchCruiseApp-API/Application/UseCases/CruiseApplications/AddCruiseApplication/AddCruiseApplicationValidator.cs
+++ ResearchCruiseApp-API/ResearchCruiseApp-API/Application/UseCases/CruiseApplications/AddCruiseApplication/AddCruiseApplicationValidator.cs
@@ -11,18 +11,12 @@ public class AddCruiseApplicationValidator : AbstractValidator<AddCruiseApplicat
         RuleForEach(command => command.FormADto.Contracts)
             .Must(contractDto =>
             {
-                const string pdfBase64UrlPrefix = "data:application/pdf;base64,"; 
-                var pdfBase64UrlPrefixLength = pdfBase64UrlPrefix.Length;
                 const int fileHeaderLength = 4;
                 const string pdfFileHeaderString = "%PDF";
-
-                var scanContentWithoutPrefix = string.Concat(
-                    contractDto.Scan.Content.Skip(pdfBase64UrlPrefixLength)
-                );
                 
                 try
-                { 
-                    var scanBytes = Convert.FromBase64String(scanContentWithoutPrefix);
+                {
+                    var scanBytes = GetScanBytes(contractDto.Scan.Content);
                     var scanFileHeader = Encoding.ASCII.GetString(scanBytes
                         .Take(fileHeaderLength)
                         .ToArray()
@@ -36,5 +30,28 @@ public class AddCruiseApplicationValidator : AbstractValidator<AddCruiseApplicat
                 }
             })
             .WithMessage("Skan umowy musi być plikiem PDF.");
+
+        RuleForEach(command => command.FormADto.Contracts)
+            .Must(contractDto =>
+            {
+                const int maxFileSizeBytes = 2_097_152;
+                var scanBytes = GetScanBytes(contractDto.Scan.Content);
+                
+                return scanBytes.Length <= maxFileSizeBytes;
+            })
+            .WithMessage("Rozmiar skanu nie może przekraczać 2 MiB.");
+    }
+
+
+    private static byte[] GetScanBytes(string scanContent)
+    {
+        const string pdfBase64UrlPrefix = "data:application/pdf;base64,"; 
+        var pdfBase64UrlPrefixLength = pdfBase64UrlPrefix.Length;
+
+        var scanContentWithoutPrefix =  string.Concat(
+            scanContent.Skip(pdfBase64UrlPrefixLength)
+        );
+        
+        return Convert.FromBase64String(scanContentWithoutPrefix);
     }
 }
