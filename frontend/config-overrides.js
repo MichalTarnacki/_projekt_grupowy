26c9edfa8170a27fe77b3c33e97c6461f82fe3bd
Michał Tarnacki <michaltarnacki@icloud.com>
2024-10-09 01:19:32 +0200

    Prettier + file restruct p1

---
+++ frontend/config-overrides.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
+
+module.exports = function override(config) {
+    config.resolve.plugins = [
+        ...(config.resolve.plugins || []),
+        new TsconfigPathsPlugin({
+            configFile: path.resolve(__dirname, 'tsconfig.json')
+        }),
+    ];
+
+    config.resolve.extensions = ['.ts', '.tsx', '.js', '.jsx'];
+
+    return config;
+};
