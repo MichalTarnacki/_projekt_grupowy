90bb86fee3fa11c32f26739fcec6babf9a1a28b8
matowaty <matinowak24@gmail.com>
2024-10-16 10:57:23 +0200

    fix

--- frontend/src/Components/Pages/HomePage/Tiles/MyPublicationsTile.tsx
+++ frontend/src/Components/Pages/HomePage/Tiles/MyPublicationsTile.tsx
@@ -2,14 +2,14 @@ import Tile from "./Tile";
 import {Link} from "react-router-dom";
 import {Path as Path} from "../../../Tools/Path";
 import React from "react";
-import {ReactComponent as Icon} from "/node_modules/bootstrap-icons/icons/book.svg"
+import {ReactComponent as Icon} from "/node_modules/bootstrap-icons/icons/book-fill.svg"
 
 function MyPublicationsTile(){
     return (
         <Tile>
             <Link to={Path.MyPublications} className={"common-tile-link"}>
                 <Icon className={"bi-menu-common"}/>
-                Moje Publikacje
+                Moje publikacje
             </Link>
         </Tile>
     )
